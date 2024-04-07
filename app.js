fd5388ba9a1c4183c0436a93faf7ce3001ecfe66
wotex <106171903+wotex3@users.noreply.github.com>
2024-04-07 17:16:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const path = require('path');
+const path = require('path');
 
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
