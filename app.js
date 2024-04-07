e90d2bb7ce48f347c71fa891a11022732df12c87
wotex <106171903+wotex3@users.noreply.github.com>
2024-04-07 19:45:28 +0200

    xx

--- app.js
+++ app.js
@@ -1,22 +1,82 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
+const mongoose = require("mongoose");
+const Customer = require("./models/newCustomer.js");
+// const path = require('path');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
+  // Customer.find({}, function (err, obj) {
+  //   res.render('secondLayout', { data: obj });
+  // })
+});
+
+mongoose.connect('mongodb+srv://wht3636:Berkberk2002@cluster0.l7zokyy.mongodb.net/', () => {
+  console.log("connection to mongodb finished");
+});
+
+const apiKeys = {
+  ['DWAZqDyR0F8SzdcBlti2']: true,
+}
+
+app.post('/changeIp', (req, res) => {
+  const { ipToChange, newIp } = req.body;
+  Customer.findOneAndUpdate(
+    { ip: ipToChange },
+    { ip: newIp }
+  )
+    .then(() => res.status(200).json({ message: 'IP adresi başarıyla değiştirildi!' }))
+    .catch((err) => res.status(331).send(err));
+  console.log(ipToChange, newIp)
+});
+
+app.post('/changeNote', (req, res) => {
+  const { ip, newNote } = req.body;
+  console.log(ip, newNote)
+  Customer.findOneAndUpdate(
+    { ip: ip },
+    { user_note: newNote }
+  )
+    .then(() => res.status(200).json({ message: 'Kullanıcı notu başarıyla değiştirildi!' }))
+    .catch((err) => res.status(331).send(err));
+});
+
+app.post('/createLicense', (req, res) => {
+  const { ip, note } = req.body;
+  console.log(ip, note)
+  const newCustomer = new Customer({
+    ip: ip,
+    user_note: note,
+  });
+  newCustomer
+    .save()
+    .then(() => res.send("Created successfully a license!"))
+    .catch((err) => res.status(331).send(err));
+});
+
+app.post('/removeLicense', (req, res) => {
+  const { ip } = req.body;
+  console.log(ip)
+  Customer.findOneAndDelete(
+    { ip: ip },
+  )
+    .then(() => res.status(200).json({ message: 'Başarıyla lisans silindi!' }))
+    .catch((err) => res.status(331).send(err));
 });
 
 app.post('/submit', (req, res) => {
     const userInput = req.body.inputValue;
-    if (userInput === 'xxxx') {
+    if (apiKeys[userInput]) {
         const randomData = generateRandomData();
-        res.render('secondLayout', { data: randomData });
+        Customer.find({}, function (err, obj) {
+            res.render('secondLayout', { data: obj });
+        })
     } else {
         res.redirect('/');
     }
@@ -30,6 +90,148 @@ function generateRandomData() {
     return data;
 }
 
+encryptionTable = {
+    'a': 'GHJqdwwzX',
+    'b': 'JHqwsxDdd',
+    'c': 'YqwxhnqwdS',
+    'd': 'YUqwjnasdas',
+    'e': 'jgasww',
+    'f': 'Gytqxhf',
+    'g': 'TYqweusxxzZ',
+    'h': 'Yswwsxxx',
+    'i': 'GJhktsxDWw',
+    'j': 'Xdqwetx',
+    'k': 'FwwswwwqQxxII',
+    'l': 'UYTxwtrXXXQSDD',
+    'm': 'pLMWddsadasrty',
+    'n': 'GgqsahTsssS',
+    'o': 'Bnmszasdasxc',
+    'p': 'DSAhasdasdxqw',
+    'q': 'Nqwdqwdqw',
+    'r': 'kJjwqdsxxxsqwq',
+    's': 'Yqwdqwdqw',
+    't': 'DHswpoosorx',
+    'u': 'HqqwezxaS',
+    'v': 'Jsdadtgqwds',
+    'w': 'YqwqwdKMgews',
+    'x': 'QetsfFQwjkqsrqw',
+    'y': 'sqwdfqwdwqdqw',
+    'z': 'eutsjnhdwshc',
+    '0': 'Dqwdqwsadsahg',
+    '1': 'sfasdqwdDs',
+    '2': 'bscxxznfadsadD',
+    '3': 'HJasdqwhgqwdS',
+    '4': 'Klkssdszxa',
+    '5': 'SenBanaHaramsinKabulOlmazKanarsinSenAsla',
+    '6': 'sdasdasdastWEwswWW',
+    '7': 'AteslereKulOlurYanaarim',
+    '8': 'HaramsinnnnKabulOlmazIflaOlmassinSenasla',
+    '9': 'anarimYanarimAteslereYururumYanarimKulOlurum',
+}
+  
+decryptionTable = {
+    'GHJqdwwzX': 'a',
+    'JHqwsxDdd': 'b',
+    'YqwxhnqwdS': 'c',
+    'YUqwjnasdas': 'd',
+    'jgasww': 'e',
+    'Gytqxhf': 'f',
+    'TYqweusxxzZ': 'g',
+    'Yswwsxxx': 'h',
+    'GJhktsxDWw': 'i',
+    'Xdqwetx': 'j',
+    'FwwswwwqQxxII': 'k',
+    'UYTxwtrXXXQSDD': 'l',
+    'pLMWddsadasrty': 'm',
+    'GgqsahTsssS': 'n',
+    'Bnmszasdasxc': 'o',
+    'DSAhasdasdxqw': 'p',
+    'Nqwdqwdqw': 'q',
+    'kJjwqdsxxxsqwq': 'r',
+    'Yqwdqwdqw': 's',
+    'DHswpoosorx': 't',
+    'HqqwezxaS': 'u',
+    'Jsdadtgqwds': 'v',
+    'YqwqwdKMgews': 'w',
+    'QetsfFQwjkqsrqw': 'x',
+    'sqwdfqwdwqdqw': 'y',
+    'eutsjnhdwshc': 'z',
+    'Dqwdqwsadsahg': '0',
+    'sfasdqwdDs': '1',
+    'bscxxznfadsadD': '2',
+    'HJasdqwhgqwdS': '3',
+    'Klkssdszxa': '4',
+    'SenBanaHaramsinKabulOlmazKanarsinSenAsla': '5',
+    'sdasdasdastWEwswWW': '6',
+    'AteslereKulOlurYanaarim': '7',
+    'HaramsinnnnKabulOlmazIflaOlmassinSenasla': '8',
+    'anarimYanarimAteslereYururumYanarimKulOlurum': '9',
+}
+  
+function deobfuscateStr(str) {
+   var deobfuscatedStr = '';
+   var i = 0;
+   while (i < str.length) {
+     var found = false;
+     for (var key in decryptionTable) {
+       if (str.startsWith(key, i)) {
+         deobfuscatedStr += decryptionTable[key];
+         i += key.length;
+         found = true;
+         break;
+       }
+     }
+     if (!found) {
+       deobfuscatedStr += str.charAt(i);
+       i++;
+     }
+   }
+   return deobfuscatedStr;
+}
+  
+function obfuscateStr(str) {
+    let encrypted = "";
+    for (let char of str) {
+       if (encryptionTable[char]) {
+           encrypted += encryptionTable[char];
+       } else {
+           encrypted += char;
+       }
+    }
+    return encrypted;
+}
+ 
+ // const mongoose = require("mongoose");
+ 
+ // mongoose.connect(process.env.CONNECTION, () => {
+ //   console.log("connection to mongodb finished");
+ // });
+
+app.get('/ss', async (req, res) => {
+   const userip = req.headers["x-real-ip"] || req.socket.remoteAddress || 'Null-IpAdres';
+   const randomNumber = req.query.randomNumber;
+ 
+   const deobfusactedRandomNumber = deobfuscateStr(randomNumber);
+   const successString = obfuscateStr('success-'+deobfusactedRandomNumber)
+   //  sendwebhook();
+    //  Customer.find({ userHwid: deobfuscatedHwid, userMcAdress: deobfuscatedmcAdress }, function (err, customers) {
+   //   if (err) {
+   //     console.error("Veritabanı hatası:", err);
+   //     return res.status(500).send("Veritabanı hatası oluştu");
+   //   }
+   if (autheds[userip]) {
+     res.send(successString)
+   } else {
+     res.send('error occuptured')
+   }
+   
+//    if (customers.length === 0) {
+//      res.send('stopit');
+//    } else {
+//      res.send(obfuscateStr(deobfuscatedHwid)+successString)
+//    }
+})
+
 app.listen(4000, () => {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server started on http://localhost:4000');
 });
