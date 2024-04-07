8f28465c656b88c968f898a879465ae566d45e3d
wotex <106171903+wotex3@users.noreply.github.com>
2024-04-07 05:20:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,128 +1,21 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const mongoose = require("mongoose");
 const Customer = require("../models/newCustomer.js");
 
 const port = 5000
 app.use(express.json());
-
 app.use(cors())
 
-encryptionTable = {
-   'a': 'GHJqdwwzX',
-   'b': 'JHqwsxDdd',
-   'c': 'YqwxhnqwdS',
-   'd': 'YUqwjnasdas',
-   'e': 'jgasww',
-   'f': 'Gytqxhf',
-   'g': 'TYqweusxxzZ',
-   'h': 'Yswwsxxx',
-   'i': 'GJhktsxDWw',
-   'j': 'Xdqwetx',
-   'k': 'FwwswwwqQxxII',
-   'l': 'UYTxwtrXXXQSDD',
-   'm': 'pLMWddsadasrty',
-   'n': 'GgqsahTsssS',
-   'o': 'Bnmszasdasxc',
-   'p': 'DSAhasdasdxqw',
-   'q': 'Nqwdqwdqw',
-   'r': 'kJjwqdsxxxsqwq',
-   's': 'Yqwdqwdqw',
-   't': 'DHswpoosorx',
-   'u': 'HqqwezxaS',
-   'v': 'Jsdadtgqwds',
-   'w': 'YqwqwdKMgews',
-   'x': 'QetsfFQwjkqsrqw',
-   'y': 'sqwdfqwdwqdqw',
-   'z': 'eutsjnhdwshc',
-   '0': 'Dqwdqwsadsahg',
-   '1': 'sfasdqwdDs',
-   '2': 'bscxxznfadsadD',
-   '3': 'HJasdqwhgqwdS',
-   '4': 'Klkssdszxa',
-   '5': 'SenBanaHaramsinKabulOlmazKanarsinSenAsla',
-   '6': 'sdasdasdastWEwswWW',
-   '7': 'AteslereKulOlurYanaarim',
-   '8': 'HaramsinnnnKabulOlmazIflaOlmassinSenasla',
-   '9': 'anarimYanarimAteslereYururumYanarimKulOlurum',
- }
- 
- decryptionTable = {
-   'GHJqdwwzX': 'a',
-   'JHqwsxDdd': 'b',
-   'YqwxhnqwdS': 'c',
-   'YUqwjnasdas': 'd',
-   'jgasww': 'e',
-   'Gytqxhf': 'f',
-   'TYqweusxxzZ': 'g',
-   'Yswwsxxx': 'h',
-   'GJhktsxDWw': 'i',
-   'Xdqwetx': 'j',
-   'FwwswwwqQxxII': 'k',
-   'UYTxwtrXXXQSDD': 'l',
-   'pLMWddsadasrty': 'm',
-   'GgqsahTsssS': 'n',
-   'Bnmszasdasxc': 'o',
-   'DSAhasdasdxqw': 'p',
-   'Nqwdqwdqw': 'q',
-   'kJjwqdsxxxsqwq': 'r',
-   'Yqwdqwdqw': 's',
-   'DHswpoosorx': 't',
-   'HqqwezxaS': 'u',
-   'Jsdadtgqwds': 'v',
-   'YqwqwdKMgews': 'w',
-   'QetsfFQwjkqsrqw': 'x',
-   'sqwdfqwdwqdqw': 'y',
-   'eutsjnhdwshc': 'z',
-   'Dqwdqwsadsahg': '0',
-   'sfasdqwdDs': '1',
-   'bscxxznfadsadD': '2',
-   'HJasdqwhgqwdS': '3',
-   'Klkssdszxa': '4',
-   'SenBanaHaramsinKabulOlmazKanarsinSenAsla': '5',
-   'sdasdasdastWEwswWW': '6',
-   'AteslereKulOlurYanaarim': '7',
-   'HaramsinnnnKabulOlmazIflaOlmassinSenasla': '8',
-   'anarimYanarimAteslereYururumYanarimKulOlurum': '9',
- }
- 
-function deobfuscateStr(str) {
-  var deobfuscatedStr = '';
-  var i = 0;
-  while (i < str.length) {
-    var found = false;
-    for (var key in decryptionTable) {
-      if (str.startsWith(key, i)) {
-        deobfuscatedStr += decryptionTable[key];
-        i += key.length;
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      deobfuscatedStr += str.charAt(i);
-      i++;
-    }
-  }
-  return deobfuscatedStr;
-}
- 
-function obfuscateStr(str) {
-  let encrypted = "";
-  for (let char of str) {
-      if (encryptionTable[char]) {
-          encrypted += encryptionTable[char];
-      } else {
-          encrypted += char;
-      }
-  }
-  return encrypted;
-}
 
-const mongoose = require("mongoose");
 
 mongoose.connect('mongodb+srv://wht3636:Berkberk2002@cluster0.l7zokyy.mongodb.net/', () => {
   console.log("connection to mongodb finished");
+  const newCustomer = new Customer({
+    ip: '83.251.50.65',
+    userNote: 'user note',
+  });
 });
 
 const apiKeys = {
@@ -156,15 +49,118 @@ app.get('/ss', async (req, res) => {
   } else {
     res.send('error occuptured')
   }
-  
-  //   if (customers.length === 0) {
-  //     res.send('stopit');
-  //   } else {
-  //     res.send(obfuscateStr(deobfuscatedHwid)+successString)
-  //   }
-
-  //  })
 })
+
+encryptionTable = {
+  'a': 'GHJqdwwzX',
+  'b': 'JHqwsxDdd',
+  'c': 'YqwxhnqwdS',
+  'd': 'YUqwjnasdas',
+  'e': 'jgasww',
+  'f': 'Gytqxhf',
+  'g': 'TYqweusxxzZ',
+  'h': 'Yswwsxxx',
+  'i': 'GJhktsxDWw',
+  'j': 'Xdqwetx',
+  'k': 'FwwswwwqQxxII',
+  'l': 'UYTxwtrXXXQSDD',
+  'm': 'pLMWddsadasrty',
+  'n': 'GgqsahTsssS',
+  'o': 'Bnmszasdasxc',
+  'p': 'DSAhasdasdxqw',
+  'q': 'Nqwdqwdqw',
+  'r': 'kJjwqdsxxxsqwq',
+  's': 'Yqwdqwdqw',
+  't': 'DHswpoosorx',
+  'u': 'HqqwezxaS',
+  'v': 'Jsdadtgqwds',
+  'w': 'YqwqwdKMgews',
+  'x': 'QetsfFQwjkqsrqw',
+  'y': 'sqwdfqwdwqdqw',
+  'z': 'eutsjnhdwshc',
+  '0': 'Dqwdqwsadsahg',
+  '1': 'sfasdqwdDs',
+  '2': 'bscxxznfadsadD',
+  '3': 'HJasdqwhgqwdS',
+  '4': 'Klkssdszxa',
+  '5': 'SenBanaHaramsinKabulOlmazKanarsinSenAsla',
+  '6': 'sdasdasdastWEwswWW',
+  '7': 'AteslereKulOlurYanaarim',
+  '8': 'HaramsinnnnKabulOlmazIflaOlmassinSenasla',
+  '9': 'anarimYanarimAteslereYururumYanarimKulOlurum',
+}
+
+decryptionTable = {
+  'GHJqdwwzX': 'a',
+  'JHqwsxDdd': 'b',
+  'YqwxhnqwdS': 'c',
+  'YUqwjnasdas': 'd',
+  'jgasww': 'e',
+  'Gytqxhf': 'f',
+  'TYqweusxxzZ': 'g',
+  'Yswwsxxx': 'h',
+  'GJhktsxDWw': 'i',
+  'Xdqwetx': 'j',
+  'FwwswwwqQxxII': 'k',
+  'UYTxwtrXXXQSDD': 'l',
+  'pLMWddsadasrty': 'm',
+  'GgqsahTsssS': 'n',
+  'Bnmszasdasxc': 'o',
+  'DSAhasdasdxqw': 'p',
+  'Nqwdqwdqw': 'q',
+  'kJjwqdsxxxsqwq': 'r',
+  'Yqwdqwdqw': 's',
+  'DHswpoosorx': 't',
+  'HqqwezxaS': 'u',
+  'Jsdadtgqwds': 'v',
+  'YqwqwdKMgews': 'w',
+  'QetsfFQwjkqsrqw': 'x',
+  'sqwdfqwdwqdqw': 'y',
+  'eutsjnhdwshc': 'z',
+  'Dqwdqwsadsahg': '0',
+  'sfasdqwdDs': '1',
+  'bscxxznfadsadD': '2',
+  'HJasdqwhgqwdS': '3',
+  'Klkssdszxa': '4',
+  'SenBanaHaramsinKabulOlmazKanarsinSenAsla': '5',
+  'sdasdasdastWEwswWW': '6',
+  'AteslereKulOlurYanaarim': '7',
+  'HaramsinnnnKabulOlmazIflaOlmassinSenasla': '8',
+  'anarimYanarimAteslereYururumYanarimKulOlurum': '9',
+}
+
+function deobfuscateStr(str) {
+ var deobfuscatedStr = '';
+ var i = 0;
+ while (i < str.length) {
+   var found = false;
+   for (var key in decryptionTable) {
+     if (str.startsWith(key, i)) {
+       deobfuscatedStr += decryptionTable[key];
+       i += key.length;
+       found = true;
+       break;
+     }
+   }
+   if (!found) {
+     deobfuscatedStr += str.charAt(i);
+     i++;
+   }
+ }
+ return deobfuscatedStr;
+}
+
+function obfuscateStr(str) {
+ let encrypted = "";
+ for (let char of str) {
+     if (encryptionTable[char]) {
+         encrypted += encryptionTable[char];
+     } else {
+         encrypted += char;
+     }
+ }
+ return encrypted;
+}
 
 app.listen(port, () => {
    console.log(`Server ${port} portundan başlatıldı!`)
